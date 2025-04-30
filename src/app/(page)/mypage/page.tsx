7a5d0a2a18f3cd9b9b106de5b5c61d56982e9529
kimjunsu <junsu4621@naver.com>
2025-04-30 10:20:00 +0900

    [API] 유저 프로필 api연동

--- src/app/(page)/mypage/page.tsx
+++ src/app/(page)/mypage/page.tsx
@@ -6,8 +6,51 @@ import MyPage_ProfileCard from "@/app/components/mypage/MyPage_ProfileCard";
 import MyPage_GoalSection from "@/app/components/mypage/MyPage_GoalSection";
 import MyPage_Settings from "@/app/components/mypage/MyPage_Settings";
 import TabBar from "@/app/components/common/TabBar";
+import { useEffect } from "react";
+import { useProfileSetupStore } from "@/app/store/useProfileSetupStore";
+import axios from "@/app/lib/axios";
 
 const MyPage = () => {
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const res = await axios.get(
+                    "https://api.eatfit.site/api/core/users/profile"
+                );
+                const {
+                    name,
+                    gender,
+                    birthYear,
+                    height,
+                    weight,
+                    goalCategory,
+                    targetWeight,
+                    disease,
+                } = res.data.data;
+
+                // 나이 계산산
+                const currentYear = new Date().getFullYear(); // 현재 년도
+                const age = (currentYear - birthYear).toString(); // 나이 계산
+                const diseases = disease ? disease.split(",") : []; // 질병은 배열로 관리
+
+                // 스토어에 상태 업데이트트
+                const store = useProfileSetupStore.getState();
+                store.setNickname(name);
+                store.setGender(gender);
+                store.setAge(age);
+                store.setHeight(height.toString());
+                store.setWeight(weight.toString());
+                store.setTargetWeight(targetWeight.toString());
+                store.setPurpose(goalCategory);
+                store.setDiseases(diseases);
+                console.log("✅ 마이페이지 프로필 로딩 성공: ", res.data.data);
+            } catch (err) {
+                console.error("❌ 프로필 조회 실패", err);
+            }
+        };
+        fetchProfile();
+    }, []);
+
     return (
         <Box>
             <MyPage_Header />
