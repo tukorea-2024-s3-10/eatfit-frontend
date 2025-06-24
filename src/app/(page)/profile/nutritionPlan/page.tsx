9bc294a81fb477ecf3d43591e4cb22327e101434
김준수 <junsu4621@naver.com>
2025-06-24 10:31:21 +0900

    2

--- src/app/(page)/profile/nutritionPlan/page.tsx
+++ src/app/(page)/profile/nutritionPlan/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import ProfileSetupHeader from "@/app/components/profileSetup/Profile_Setup_Header";
 import NutritionPlan_RecommendedCalorie from "@/app/components/nutritionPlan/NutritionPlan_RecommendedCalorie";
 import NutritionPlan_CalorieEditor from "@/app/components/nutritionPlan/NutritionPlan_CalorieEditor";
@@ -6,20 +8,30 @@ import NutritionPlan_TargetWeightEditor from "@/app/components/nutritionPlan/Nut
 import NutritionPlan_NutrientSummary from "@/app/components/nutritionPlan/NutritionPlan_NutrientSummary";
 import NutritionPlan_PersonalizedInfo from "@/app/components/nutritionPlan/NutritionPlan_PersonalizedInfo";
 import NutritionPlan_GoToManageButton from "@/app/components/nutritionPlan/NutritionPlan_GoToManageButton";
-const page = () => {
-    return (
-        <div>
-            <ProfileSetupHeader />
-            <NutritionPlan_RecommendedCalorie />
-            <NutritionPlan_CalorieEditor />
-            <div className="my-10">
-                <NutritionPlan_TargetWeightEditor />
-            </div>
-            <NutritionPlan_NutrientSummary />
-            <NutritionPlan_PersonalizedInfo />
-            <NutritionPlan_GoToManageButton />
-        </div>
-    );
+
+const Page = () => {
+  const [refreshCount, setRefreshCount] = useState(0);
+
+  const handleNutrientRefresh = () => {
+    setRefreshCount(prev => prev + 1);
+  };
+
+  return (
+    <div>
+      <ProfileSetupHeader />
+      <NutritionPlan_RecommendedCalorie />
+      <NutritionPlan_CalorieEditor />
+
+      <div className="my-10">
+        {/* ✅ 목표 체중 수정 후 onSubmit 실행 시 영양소 요약 새로고침 */}
+        <NutritionPlan_TargetWeightEditor onSubmit={handleNutrientRefresh} />
+      </div>
+
+      <NutritionPlan_NutrientSummary refreshTrigger={refreshCount} />
+      <NutritionPlan_PersonalizedInfo />
+      <NutritionPlan_GoToManageButton />
+    </div>
+  );
 };
 
-export default page;
+export default Page;
